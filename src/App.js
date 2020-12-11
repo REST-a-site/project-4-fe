15d50c882e4321ec3622f78908c87d6e55caf56b
jake-utk <jakeryanaddis@gmail.com>
2020-12-11 10:57:45 -0600

    Added Home.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Home from './components/Home/Home'
 
 const App = () => {
-    return <div>Hello world</div>
+    return <Home />
 }
 
 export default App
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	background-color: orange;
+`;
+
+const Home = () => {
+	return <Container>Hello from Home.js!</Container>;
+};
+
+export default Home;
