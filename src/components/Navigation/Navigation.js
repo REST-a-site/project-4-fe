986b6746d1171ec5fdc4570873a90d1558824850
jake-utk <jakeryanaddis@gmail.com>
2020-12-14 08:45:08 -0600

    Added styled-components to nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,37 @@
 import React from "react";
-// import styled from "styled-components";
-import { Container } from "../Styles/index";
+import styled, { css } from "styled-components";
 
-const Navigation = () => {
-	return <Container>Navigation Bar Here!</Container>;
+const NavContainer = styled.div`
+	overflow: hidden;
+	onscroll=${{myFunction() {
+		let sticky = NavContainer.offsetTop
+		if (window.pageYOffset >= sticky) {
+			css`
+				position: fixed;
+				top: 0;
+				width: 100%;
+			`
+		}
+	}}}
+
+`
+
+const Link = styled.a`
+	float: center;
+	margin: 5%;
+	text-align: center;
+`
+
+
+
+const Navigation = ({ navStyle, setNavStyle }) => {
+	return (
+		<NavContainer navStyle={navStyle} setNavStyle={setNavStyle} >
+			<Link href=''>Home</Link>
+			<Link href=''>Menu</Link>
+			<Link href=''>Events</Link>
+		</NavContainer>
+	);
 };
 
 export default Navigation;
