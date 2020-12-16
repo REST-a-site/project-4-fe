27b6caf95d2aa6c849671b7422cf081a81d4a841
jake-utk <jakeryanaddis@gmail.com>
2020-12-16 00:19:04 -0600

    Added video, reworked styled components for hero, migrating over css now

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import Video from '../../videos/video.mp4'
+import styled from 'styled-components'
+import { Button } from '../Styles'
+
+const Container = styled.div``
+
+const Background = styled.div``
+
+const VideoImport = styled.video``
+
+const Content = styled.div``
+
+const H1 = styled.h1``
+
+const P = styled.p``
+
+const Wrapper = styled.div``
+
 
 
 const Hero = () => {
+
+
     return (
-        <div>
-            Hero here
-        </div>
+        <Container>
+            <Background>
+                <VideoImport autoPlay loop muted src={Video} type='video/mp4' />
+            </Background>
+            <Content>
+                <H1>921steak</H1>
+                <P>best steak in the game</P>
+                <Wrapper>
+                    <Button to='reserve' />
+                </Wrapper>
+            </Content>
+            
+        </Container>
     )
 }
 
