3c0bd69edd88bcecf32b083bd1ba0ee7528a63e7
jake-utk <jakeryanaddis@gmail.com>
2020-12-15 23:50:40 -0600

    Scaffolded basic components from restructuring, set up new home page

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            Navigation Bar Here
+        </div>
+    )
+}
+
+export default Nav
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Contact from '../components/Contact'
+import Info from '../components/Info'
+import Reserve from '../components/Reserve'
+import Navigation from '../components/Navigation'
+import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
         <>
+        <Sidebar />
+        <Navigation />
         <Hero />
+        <Reserve />
+        <Info />
+        <Contact />
+        <Info />
+        <Footer />
+
         </>
     )
 }
