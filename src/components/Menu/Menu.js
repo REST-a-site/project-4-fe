e0e24327c1dcfa2940be34ac93da5308975474b0
jtomassoni <jtomassoni@gmail.com>
2020-12-15 17:13:57 -0700

    basic menu framework, might need to re-think the backend

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { GlobalStyle } from '../Styles/index';
+import Axios from 'axios';
 
+let url = 'https://warm-fjord-68987.herokuapp.com/api/menu';
 const Menu = () => {
-    return (
-        <div>
-            Hello from Menu.js
-        </div>
-    );
+	//will need state
+	const [menu, setMenu] = useState();
+	//will need useEffect for Axios
+	useEffect(() => {
+		Axios(url)
+			.then((res) => {
+				setMenu(res.data);
+			})
+			.catch((error) => {});
+	}, []);
+
+	//will need a lazy loader
+	if (!menu) {
+		return 'Loading...';
+	}
+	return (
+		<>
+			<GlobalStyle />
+			{console.log(menu[0].menu_item_names)}
+			<h2>{menu[0].menu_name}</h2>
+			<h4>{menu[0].menu_description}</h4>
+			<div>
+				{menu[0].menu_item_names.map((el) => (
+					<>
+                        <h1>{el.item_name}</h1>
+                        <h3>{el.price}</h3>
+						<h5>{el.item_description}</h5>
+					</>
+				))}
+			</div>
+			Appetizers Salads
+		</>
+	);
 };
 
 export default Menu;
