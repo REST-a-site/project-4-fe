c46a5b52d2bee2e243770e5c8e98a2610cb77612
jake-utk <jakeryanaddis@gmail.com>
2020-12-16 02:20:03 -0600

    Scaffolding of new nav bar done

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,9 +2,40 @@ import React from 'react'
 
 const Nav = () => {
     return (
-        <div>
-            Navigation Bar Here
-        </div>
+        <Nav>
+            <Container>
+                <Logo to='/'>921</Logo>
+                <BurgerButton>
+                    burger menu here
+                </BurgerButton>
+                <BurgerMenu>
+                    <BurgerItem>
+                        <BurgerRouter to='/'>Home</BurgerRouter>
+                    </BurgerItem>
+                    <BurgerItem>
+                        <BurgerRouter to='/menu'>Menu</BurgerRouter>
+                    </BurgerItem>
+                    <BurgerItem>
+                        <BurgerRouter to='/events'>Private Events</BurgerRouter>
+                    </BurgerItem>
+                    <BurgerItem>
+                        <BurgerLink to='reservations' exact='true'>Reservations</BurgerLink>
+                    </BurgerItem>
+                    <BurgerItem>
+                        <BurgerLink to='about' exact='true'>About</BurgerLink>
+                    </BurgerItem>
+                    <BurgerItem>
+                        <BurgerLink to='contact' exact='true'>Contact</BurgerLink>
+                    </BurgerItem>
+                    <BurgerItem>
+                        <BurgerLink to='team' exact='true'>Team</BurgerLink>
+                    </BurgerItem>
+                </BurgerMenu>
+                <LogInButton>
+                    <LogInLink to='/login' exact='true'>LogIn</LogInLink>
+                </LogInButton>
+            </Container>
+        </Nav>
     )
 }
 
