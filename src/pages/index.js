75387305845a815cdad264f285797d90d9bad6f5
jake-utk <jakeryanaddis@gmail.com>
2020-12-16 03:23:06 -0600

    Added state to index.js page for nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hero from '../components/Hero'
 import Contact from '../components/Contact'
 import Info from '../components/Info'
@@ -8,10 +8,16 @@ import Sidebar from '../components/Sidebar'
 import Footer from '../components/Footer'
 
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
-        <Sidebar />
-        <Navigation />
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <Navigation toggle={toggle} />
         <Hero />
         <Reserve />
         <Info />
