9b1895cc01cf25cfff4ff89256c1e9506fb18dc5
jake-utk <jakeryanaddis@gmail.com>
2020-12-16 03:32:33 -0600

    Links set up

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,39 +1,47 @@
 import React from "react";
-import styled from 'styled-components'
-import { FaTimes } from "react-icons/fa"
-import { Link as LinkScroll } from 'react-scroll'
-import { Link as LinkRouter } from 'react-router-dom'
+import styled from "styled-components";
+import { FaTimes } from "react-icons/fa";
+import { Link as LinkScroll } from "react-scroll";
+import { Link as LinkRouter } from "react-router-dom";
 
-const Container = styled.aside``
+const Container = styled.aside``;
 
-const Icon = styled.div``
+const Icon = styled.div``;
 
-const CloseIcon = styled(FaTimes)``
+const CloseIcon = styled(FaTimes)``;
 
-const Wrapper = styled.div``
+const Wrapper = styled.div``;
 
-const Menu = styled.ul`
-`
-const MenuLink = styled(LinkScroll)``
+const Menu = styled.ul``;
 
-const LogIn = styled.div``
+const MenuLinkScroll = styled(LinkScroll)``;
 
-const LogInRoute = styled(LinkRouter)``
+const MenuLinkRouter = styled(LinkRouter)``;
 
-const Sidebar = () => {
+const LogIn = styled.div``;
+
+const LogInRoute = styled(LinkRouter)``;
+
+const Sidebar = ({ isOpen, toggle }) => {
 	return (
-		<Container>
-			<Icon>
+		<Container isOpen={isOpen} onClick={toggle}>
+			<Icon onClick={toggle}>
 				<CloseIcon />
 			</Icon>
 			<Wrapper>
-                <Menu>
-                    <MenuLink to=''></MenuLink>
-                </Menu>
-            </Wrapper>
-            <LogIn>
-                <LogInRoute>LogIn</LogInRoute>
-            </LogIn>
+				<Menu>
+                    <MenuLinkRouter to='/' onClick={toggle}>Home</MenuLinkRouter>
+                    <MenuLinkRouter to='/events' onClick={toggle}>Events</MenuLinkRouter>
+                    <MenuLinkRouter to='/menu' onClick={toggle}>Menu</MenuLinkRouter>
+					<MenuLinkScroll to='reservations' onClick={toggle}>Reservations</MenuLinkScroll>
+					<MenuLinkScroll to='about' onClick={toggle}>About</MenuLinkScroll>
+					<MenuLinkScroll to='contact' onClick={toggle}>Contact</MenuLinkScroll>
+					<MenuLinkScroll to='team' onClick={toggle}>Team</MenuLinkScroll>
+				</Menu>
+			</Wrapper>
+			<LogIn>
+				<LogInRoute to='/login'>LogIn</LogInRoute>
+			</LogIn>
 		</Container>
 	);
 };
