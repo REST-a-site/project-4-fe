26b72729b81deab955e950a91e5778781e94f7ac
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2020-12-17 15:58:14 -0600

    menu items successfully displaying

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 // import styled from 'styled-components';
 import Axios from 'axios';
 
-let url = 'https://warm-fjord-68987.herokuapp.com/api/menu';
+let url = 'http://restasitebackend.herokuapp.com/api/menu';
 const Menu = () => {
-	//will need state
 	const [menu, setMenu] = useState();
-	//will need useEffect for Axios
+
 	useEffect(() => {
 		Axios(url)
 			.then((res) => {
@@ -15,35 +14,32 @@ const Menu = () => {
 			.catch((error) => {});
 	}, []);
 
-	//will need a lazy loader
 	if (!menu) {
 		return 'Loading...';
 	}
+	console.log(menu)
 	return (
 		<>
 			{menu.map((el) => {
 				return (
 					<>
-						<h1>{el.menu_name}</h1>
-						<h4>{el.menu_description}</h4>
-						{el.menu_item_names.map((el) => {
-							return (
-								<>
-									{/* Here is the first section of food
-									Only render this section if the menu_section === "Appetizers" */}
-									Appetizers
-									<h3>{el.item_name}</h3>
-									<h4>{el.item_description}</h4>
-									<p> Active: {el.item_active ? 'Yes' : 'No'}</p>
-									Shellfish
-								</>
-							);
-						})}
+					<h1>{el.section_name}</h1>
+					{el.menu_item.map((el) => {
+						return (
+							<>
+							<h2>{el.menu_section}</h2>
+							<h3>{el.item_name}</h3>
+							<p>{el.item_description}</p>
+							<p>{el.price}</p>
 					</>
-				);
-			})}
+						)
+					})};
+				</>
+				)
+			})};
 		</>
 	);
 };
 
 export default Menu;
+
