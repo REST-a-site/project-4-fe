95d91d40921a940938b46287951490ed02be7847
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2020-12-17 17:15:59 -0600

    all menu sections rendering

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,45 +1,15 @@
-import React, { useState, useEffect } from 'react';
-// import styled from 'styled-components';
-import Axios from 'axios';
+import React from 'react';
+import Menu from '../components/Menu';
 
-let url = 'http://restasitebackend.herokuapp.com/api/menu';
-const Menu = () => {
-	const [menu, setMenu] = useState();
+const MenuPage = () => {
 
-	useEffect(() => {
-		Axios(url)
-			.then((res) => {
-				setMenu(res.data);
-			})
-			.catch((error) => {});
-	}, []);
-
-	if (!menu) {
-		return 'Loading...';
-	}
-	console.log(menu)
 	return (
 		<>
-			{menu.map((el) => {
-				return (
-					<>
-					<h1>{el.section_name}</h1>
-					{el.menu_item.map((el) => {
-						return (
-							<>
-							<h2>{el.menu_section}</h2>
-							<h3>{el.item_name}</h3>
-							<p>{el.item_description}</p>
-							<p>{el.price}</p>
-					</>
-						)
-					})};
-				</>
-				)
-			})};
+			<Menu />
 		</>
+
 	);
 };
 
-export default Menu;
+export default MenuPage;
 
