b06432d226c5d0b37c958f4e8c58556b13e9ef25
jtomassoni <jtomassoni@gmail.com>
2021-12-03 19:53:48 -0700

    menu up to date with main

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Axios from 'axios';
-
-let url = 'https://warm-fjord-68987.herokuapp.com/api/menu';
-const Menu = () => {
-	//will need state
-	const [menu, setMenu] = useState();
-	//will need useEffect for Axios
-	useEffect(() => {
-		Axios(url)
-			.then((res) => {
-				setMenu(res.data);
-			})
-			.catch((error) => {});
-	}, []);
-
-	//will need a lazy loader
-	if (!menu) {
-		return 'Loading...';
-	}
-	return (
-		<>
-			{menu.map((el) => {
-				return (
-					<>
-						<h1>{el.menu_name}</h1>
-						<h4>{el.menu_description}</h4>
-						{el.menu_item_names.map((el) => {
-							return (
-								<>
-									{/* Here is the first section of food
-									Only render this section if the menu_section === "Appetizers" */}
-									Appetizers
-									<h3>{el.item_name}</h3>
-									<h4>{el.item_description}</h4>
-									<p> Active: {el.item_active ? 'Yes' : 'No'}</p>
-									Shellfish
-								</>
-							);
-						})}
-					</>
-				);
-			})}
-=======
 import React, { useState } from "react";
 import Menu from "../components/Menu";
 import Sidebar from "../components/Sidebar";
@@ -62,7 +16,6 @@ const MenuPage = () => {
 			<Navigation toggle={toggle} />
 			<Menu />
 			<Footer />
->>>>>>> main
 		</>
 	);
 };
