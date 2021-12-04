50b1f524a1bb31109971a75e8485307370f083a6
jtomassoni <jtomassoni@gmail.com>
2021-12-03 20:15:09 -0700

    top layer menu object fix

--- src/components/MenuSection/index.js
+++ src/components/MenuSection/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import MenuItem from "../MenuItem";
-import styled from "styled-components";
+import React from 'react';
+import MenuItem from '../MenuItem';
+import styled from 'styled-components';
 
 const Container = styled.div``;
 
@@ -11,11 +11,11 @@ const MenuSection = ({ menu, section }) => {
 		<Container>
 			<SectionName>{section}</SectionName>
 
-			{menu[0].menu_item_name.map((el, i) => {
+			{/* {menu[0].menu_item_name.map((el, i) => {
 				return menu[0].menu_item_name[i].menu_section === section ? (
 					<MenuItem key={i} index={i} item={menu[0].menu_item_name[i]} />
 				) : null;
-			})}
+			})} */}
 		</Container>
 	);
 };
